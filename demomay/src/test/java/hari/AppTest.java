132e3cd173a2125fd2e1a5521c49d1a93f6b3d76
krish <krish888@gmail.com>
2019-05-28 20:34:14 +0530

    adding maven

---
+++ demomay/src/test/java/hari/AppTest.java
@@ -0,0 +1,20 @@
+package hari;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
